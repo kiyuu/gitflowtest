69e504346479b666b3bf1b50d434fb1822411cdf
kiyu <xiukyxiglht@outlook.jp>
2017-09-24 18:38:12 +0900

    Revert "Merged in feature/vb (pull request #2)"
    This reverts commit 3fd98490ff22c2de4545844a865307ae05f940df.

--- SampleVB/SampleVB/Module1.vb
+++ SampleVB/SampleVB/Module1.vb
@@ -4,16 +4,52 @@ Module Module1
 
     Sub Main()
 
-        Dim a As Boolean = False
-        Dim b As Integer = 0
+        Dim flag As Boolean = False
 
-        If a = False Then
+        If flag = False Then
             Console.WriteLine("Hello World!!")
         End If
 
+        Dim sample As New Sample
 
-        Dim list As New List(Of Integer)
-        Console.ReadLine()
+        sample.test1 = 999
+        copyTest(sample)
+        Console.WriteLine("copyTest() sample.test1 = {0}", sample.test1)
+
+        sample.test1 = 999
+        copyTest2(sample)
+        Console.WriteLine("copyTest2() sample.test1 = {0}", sample.test1)
+
+        sample.test1 = 999
+        refineTest(sample)
+        Console.WriteLine("refineTest() sample.test1 = {0}", sample.test1)
+
+        sample.test1 = 999
+        refineTest2(sample)
+        Console.WriteLine("refineTest2() sample.test1 = {0}", sample.test1)
+
+
+        Console.Read()
+    End Sub
+
+    Sub copyTest(ByVal sample As Sample)
+        sample = New Sample()
+        sample.test1 = 1
+
+    End Sub
+
+    Sub copyTest2(ByVal sample As Sample)
+        sample.test1 = 2
+    End Sub
+
+    Sub refineTest(ByRef sample As Sample)
+        sample = New Sample()
+        sample.test1 = 3
+
+    End Sub
+
+    Sub refineTest2(ByRef sample As Sample)
+        sample.test1 = 4
     End Sub
 
 End Module
