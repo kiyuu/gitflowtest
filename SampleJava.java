7875c72fd1e687d326e5efb9f0a9e1b73bbb3fe9
kiyu <xiukyxiglht@outlook.jp>
2017-09-24 19:13:09 +0900

    値渡し参照渡しのサンプル

--- SampleJava.java
+++ SampleJava.java
@@ -16,14 +16,6 @@ public final class SampleJava{
     }
 
     public static void main(String args[]){
-      boolean a = false;
-      int b = 0;
-      if (a = false)
-      {
-        System.out.println("Hello World!!");
-      }
-      List<Integer> list = new ArrayList<Integer>();
-
       RefTypeTest sample = new RefTypeTest();
 
       sample.intValue = 999;
